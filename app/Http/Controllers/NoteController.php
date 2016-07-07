a57bc4ccdc4bc01ad71363427c2773e026820bd8
Jessica Straw <jcstraw3@gmail.com>
2016-07-07 05:09:53 -0400

    Stubs out NoteController

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -9,5 +9,25 @@ use App\Http\Requests;
 use App\Note;
 
 class NoteController extends Controller {
-    //
+
+	public function create(){
+
+	}
+
+	public function readMany(){
+
+	}
+
+	public function readOne(){
+
+	}
+
+	public function update(){
+
+	}
+
+	public function delete(){
+
+	}
+
 }
