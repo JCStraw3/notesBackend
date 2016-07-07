7b398961be023513f5bdefd0a76e1238d45d5360
Jessica Straw <jcstraw3@gmail.com>
2016-07-07 04:54:36 -0400

    Updates Note model

--- app/Note.php
+++ app/Note.php
@@ -5,5 +5,12 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Note extends Model {
-    //
+
+	protected $table = 'notes';
+
+	protected $fillable = [
+		'title',
+		'body',
+	];
+
 }
