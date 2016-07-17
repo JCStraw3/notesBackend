e363b16f0a5e098a16e18a4dc191e4976af2e3f8
Jessica Straw <jcstraw3@gmail.com>
2016-07-17 07:39:16 -0400

    Updates the create.js to use .click instead of app.post

--- public/controller/create.js
+++ public/controller/create.js
@@ -7,30 +7,32 @@
 		context.render('/view/create.template', {
 		}, function(output){
 			$('#content').html(output);
-		});
 
-	});
+			$('#create').click(function(event){
 
-	app.post('#/create', function(context){
+				var action = '/create.json';
 
-		var action = '/create.json';
+				var newNote = {
+					title: $('#title').val(),
+					body: $('#body').val(),
+				}
 
-		var newNote = {
-			title: this.params['title'],
-			body: this.params['body'],
-		};
+				$.ajax({
+					url: action,
+					method: 'post',
+					data: newNote,
+					dataType: 'json',
+					error: function(result){
+						console.log(result);
+					}
+				})
+				.done(function(context){
+					$('#title').val('');
+					$('#body').val('');
+					alert('You can created a new note.');
+				});
 
-		$.ajax({
-			url: action,
-			method: 'post',
-			data: newNote,
-			dataType: 'json',
-			error: function(result){
-				console.log(result);
-			}
-		})
-		.done(function(context){
-			alert('You can created a new note.');
+			});
 		});
 
 	});
