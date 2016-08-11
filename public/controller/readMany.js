37fd1a952fe961e2aac0182ed14b073a07f63f01
Jessica Straw <jcstraw3@gmail.com>
2016-08-10 22:33:43 -0400

    Comments out the delete a note from readMany.js controller as it is currently broken.

--- public/controller/readMany.js
+++ public/controller/readMany.js
@@ -9,6 +9,36 @@
 				notes: data,
 			}, function(output){
 				$('#content').html(output);
+
+				// TODO: Complete the delete a note.
+				// As of now, this is broken.
+
+				// Delete a note from the database.
+
+				// $('#delete').click(function(event){
+
+				// 	console.log('1');
+
+				// 	var action = '/delete.json';
+
+				// 	var deleteNote = {
+				// 		id: $('#id').val(),
+				// 		_method: 'delete',
+				// 	}
+
+				// 	$.ajax({
+				// 		url: action,
+				// 		method: 'post',
+				// 		data: deleteNote,
+				// 		error: function(result){
+				// 			console.log(result);
+				// 		}
+				// 	})
+				// 	.done(function(context){
+				// 		window.location = '#/'
+				// 	});
+
+				// });
 			});
 		});
 		
